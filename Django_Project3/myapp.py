f2ff241c0bfa51a3e194c88a6a9e871fbaf7a16f
Snehal <your-email@example.com>
2026-01-08 14:35:11 +0530

    Django Project

---
+++ Django_Project3/myapp.py
@@ -0,0 +1,49 @@
+import requests
+import io
+
+
+URL = "http://127.0.0.1:8000/studentapi/"
+
+def get_data(id = None):
+   params={}
+   if id is not None:
+    params ={'id':id}
+
+    
+   r =requests.get( URL, params=params)
+   data = r.json()
+   print(data)
+    
+#get_data(1)
+
+def post_data():
+   data ={
+      'name':'Ravi',
+      'roll': 104,
+      'city':'Dhanbad'
+   }
+     
+   r = requests.post(URL, json=data)
+   print(r.json())
+   
+#post_data()
+   
+def update_data():
+   data ={
+      'id':4,
+      'name':'Soham',
+      'city':'Amravati'
+   }
+     
+   r = requests.put(URL, json=data)
+   print(r.json())
+   
+#update_data()
+
+def delete_data():
+   params ={'id':4}
+     
+   r = requests.delete(URL, params=params)
+   print(r.json())
+   
+delete_data()
